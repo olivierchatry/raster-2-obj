3d68e3b45537a656d4deef550bff3049fa3835c4
Olivier Chatry <olivier@xvision.no>
2016-07-26 09:18:07 +0200

    x fixed uniqueness of points not working

--- bin/raster-2-obj.js
+++ bin/raster-2-obj.js
@@ -8,104 +8,115 @@ const path  = require('path')
 const fs = require('fs')
 const delaunay = require('delaunay')
 const poly2tri = require('poly2tri')
+const potrace = `${__dirname}${path.sep}..${path.sep}tools${path.sep}potrace`
+const polygontoOBJ = function (geometry, opts) {
+	opts.OBJ += `o ${opts.fileName}_object_${opts.count}\n`							
+	let   id = 0
+	
+	const points 	= geometry.coordinates[0].map(
+		p => { return {x:p[0], y:p[1]} }
+	).filter(
+		(it, index, ar) => ar.find( 
+				(cur) => (it !== cur) && (cur.x === it.x) && (cur.y === it.y)
+			) === undefined
+	)
 
-const potrace = `.${path.sep}tools${path.sep}potrace`
-const depth = argv.depth || 10
-const scale = argv.scale || 1
+	points.forEach(
+		p => p.id = ++id
+	)
+
+	const pointsLength = points.length
+
+	poly2tri.noConflict()
+	const swctx = new poly2tri.SweepContext(points, {cloneArrays:true})
+	try {
+		swctx.triangulate()
+	} catch (ex) {
+		return
+	}
+	const tris 		= swctx.getTriangles()
+
+	points.forEach(
+		p => opts.OBJ += `\tv ${p.x * opts.scale} ${p.y * opts.scale} 0\n`
+	)
+	points.forEach(
+		p => opts.OBJ += `\tv ${p.x * opts.scale} ${p.y * opts.scale} ${opts.depth}\n`
+	)
+		
+	const short = (i, p, o) =>tris[i].getPoint(p).id + o
+	let 	off = opts.vertexCount
+
+	opts.OBJ += `\tg ${opts.fileName}_bottom_${opts.count}\n`
+	for (let i = 0, len = tris.length; i < len; ++i) {
+		opts.OBJ += `\tf ${short(i,0,off)} ${short(i,1,off)} ${short(i,2,off)}\n`
+	}
+
+	off = opts.vertexCount + pointsLength
+	opts.OBJ += `\tg ${opts.fileName}_top_${opts.count}\n`
+	for (let i = 0, len = tris.length; i < len; ++i) {
+		opts.OBJ += `\tf ${short(i,0,off)} ${short(i,1,off)} ${short(i,2,off)}\n`
+	}
+	
+	opts.OBJ += `\tg ${opts.fileName}_walls_${opts.count}\n`
+	opts.OBJ +=  `s 1`
+	for (let i = 0; i < pointsLength; ++i) {
+		const j = (i + 1) % pointsLength
+		const i1 = i + opts.vertexCount + 1
+		const i2 = j + opts.vertexCount + 1
+		const i3 = i1 + pointsLength
+		const i4 = i2  + pointsLength
+		opts.OBJ += `\tf ${i1} ${i2} ${i3}\n`
+		opts.OBJ += `\tf ${i3} ${i2} ${i4}\n`										
+	}
+	opts.vertexCount += pointsLength * 2	
+} 
+
 async.each(argv._, 
-	function(file, callbackEach) {
+	(file, callbackEach) => {
 		const fileOut = `${file}.json`
 		const fileOBJ = `${file}.obj`
 		const fileName = path.basename(file, path.extname(file)) 
 		async.waterfall([
-			function(callbackWaterfall) {				
+			(callbackWaterfall) => {				
 				const cmd = `${potrace} -b geojson ${file} -o ${fileOut}`
-				console.log(cmd)
-				exec(cmd, function(error, stdout, stderr) {					
+				exec(cmd, (error, stdout, stderr) => {					
 					callbackWaterfall(error)
 				})
 			},
-			function(callbackWaterfall) {
+			(callbackWaterfall) => {
 				fs.readFile(fileOut, 'utf8', callbackWaterfall)
 			},
-			function(data, callbackWaterfall) {				
+			(data, callbackWaterfall) => {
 				const parsedJSON = JSON.parse(data)
-				
-				let OBJ = ""
-				let count = 0				
-				let vertexCount = 0
-				parsedJSON.features.forEach(
-					function(feature) {						
+				const opts = {
+					count:0,
+					vertexCount:0,
+					OBJ:"",
+					fileName:fileName,
+					depth:argv.depth || 2,
+					scale:argv.scale || 1
+				}
+
+				async.each(parsedJSON.features,
+					(feature, next) => {
 						const geometry = feature.geometry
-						if (geometry && geometry.type && geometry.coordinates) {
-							switch(geometry.type) {
-								case "Polygon":		
-									OBJ += `o ${fileName}_object_${count}\n`							
-									let   id = 0
-									
-									const points 	= geometry.coordinates[0].map(
-										p => { return {x:p[0], y:p[1], id:++id}}
-									).filter(
-										(it, index, ar) => ar.find( 
-											(cur) => (cur.x === it.x) && (cur.y === it.y)
-										)
-									)
-
-									const pointsLength = points.length
-									
-									poly2tri.noConflict()
-									const swctx = new poly2tri.SweepContext(points, {cloneArrays:true})
-									swctx.triangulate()
-
-									const tris 		= swctx.getTriangles()
-
-									points.forEach(
-										p => OBJ += `\tv ${p.x * scale} ${p.y * scale} 0\n`
-									)
-									points.forEach(
-										p => OBJ += `\tv ${p.x * scale} ${p.y * scale} ${depth}\n`
-									)
-									
-									
-									const short = (i, p, o) =>tris[i].getPoint(p).id + o
-									let 	off = vertexCount
-									OBJ += `\tg ${fileName}_bottom_${count}\n`
-									for (let i = 0, len = tris.length; i < len; ++i) {
-										OBJ += `\tf ${short(i,0,off)} ${short(i,1,off)} ${short(i,2,off)}\n`
-									}
-
-									off = vertexCount + pointsLength
-									OBJ += `\tg ${fileName}_top_${count}\n`
-									for (let i = 0, len = tris.length; i < len; ++i) {
-										OBJ += `\tf ${short(i,0,off)} ${short(i,1,off)} ${short(i,2,off)}\n`
-									}
-									
-									OBJ += `\tg ${fileName}_walls_${count}\n`
-									for (let i = 0; i < pointsLength; ++i) {
-										const j = (i + 1) % pointsLength
-										const i1 = i + vertexCount + 1
-										const i2 = j + vertexCount + 1
-										const i3 = i1 + pointsLength
-										const i4 = i2  + pointsLength
-										OBJ += `\tf ${i1} ${i2} ${i3}\n`
-										OBJ += `\tf ${i3} ${i2} ${i4}\n`										
-									}
-
-									vertexCount += pointsLength * 2
-									break
-							}							
-							count++
+						if (geometry && geometry.type === "Polygon" && geometry.coordinates) {
+							polygontoOBJ(geometry, opts)
+							opts.count++
 						}
+						next()
+					},
+					() => {
+						callbackWaterfall(null, opts.OBJ)
 					}
-				)				
-				callbackWaterfall(null, OBJ)
+				)
 			},
-			function(OBJ, callbackWaterfall) {
+			(OBJ, callbackWaterfall) => {
 				fs.writeFile(fileOBJ, OBJ, callbackWaterfall)
 			}
 		], callbackEach)				
 	},
-	function (err) {
+	(err) => {
  		if( err ) {
       console.log('A file failed to process ' + err);
     } else {
